36f5fed730b9f925e77f2d50f81df1f9900ccedc
dantepli <dan932@gmail.com>
2019-01-01 21:04:30 +0200

    Added all user playlist query.

--- Moodify/Moodify/Helpers/UserPlaylistsSingleton.cs
+++ Moodify/Moodify/Helpers/UserPlaylistsSingleton.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Moodify.Model;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -32,12 +34,30 @@ namespace Moodify.Helpers
         {
             get
             {
+                this.GetPlaylistsFromDB();
                 return this.playlists;
             }
             set
             {
                 this.playlists = value;
             }
+        }
+
+        private void GetPlaylistsFromDB()
+        {
+            DBHandler handler = DBHandler.Instance;
+            int userID = 3;
+            string query = string.Format(@"SELECT playlist_affiliation.playlist_id as playlistId, playlist_name, playlist_songs.song_id as songId, title, artist_name, duration
+                            from playlist_info, playlist_affiliation, playlist_songs, song_info, artists, song_analysis
+                            where playlist_affiliation.user_id = '{0}' and playlist_affiliation.playlist_id = playlist_info.playlist_id
+                             and playlist_info.playlist_id = playlist_songs.playlist_id
+                             and song_info.song_id = playlist_songs.song_id
+                             and song_info.artist_id = artists.artist_id
+                             and song_analysis.song_id = song_info.song_id", userID);
+            JArray result = handler.ExecuteWithResults(query);
+            // TODO: Parse JSON Array.
+            List<Playlist> a = result.ToObject<List<Playlist>>();
+            int b = 5;
         }
 
         public void AddToPlaylists(Playlist playlist)
